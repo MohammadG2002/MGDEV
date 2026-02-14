01b6852cfcf736ef912d70c28b08d0a90bc186d1
MohammadG2002 <157503349+MohammadG2002@users.noreply.github.com>
2026-02-14 20:56:40 +0200

    Removed scroll-snap for tablets and phones

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,11 @@
 import "./Home.css";
 import sections from "../../components/home/sections";
+import useMobileDetection from "../../hooks/useMobileDetection";
 
 const Home = () => {
+  const isMobile = useMobileDetection();
   return (
-    <div className="home">
+    <div className={isMobile ? `home mobile` : `home`}>
       {sections.map((section) => (
         <div key={section.id} className="home__section">
           <section.component />
--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,9 +1,11 @@
 import projectSections from "../../components/projects/projectSections";
+import useMobileDetection from "../../hooks/useMobileDetection";
 import "./Projects.css";
 
 const Projects = () => {
+  const isMobile = useMobileDetection();
   return (
-    <div className="projects">
+    <div className={isMobile ? `projects mobile` : `projects`}>
       {projectSections.map((section) => (
         <div key={section.id} className="projects__section">
           <section.component />
